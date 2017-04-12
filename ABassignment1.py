bf2e05057b2853008954b14d85ac7c435760fa43
almabeganovic <alma.beganovic@stud.fh-campuswien.ac.at>
2017-04-12 10:32:06 +0200

    ready to review

--- ABassignment1.py
+++ ABassignment1.py
@@ -58,18 +58,20 @@ class Assignment1:
             if line.startswith(b'@'):
                 print("-sam_header:")
                 print (line)
-#http://pysam.readthedocs.io/en/latest/api.html
 
+    # http://pysam.readthedocs.io/en/latest/api.html
+    # For the function, .bam file must be sorted and indexed (samtools index HG00096.chrom11.ILLUMINA.bwa.GBR.low_coverage.20120522.bam )
     def get_properly_paired_reads_of_gene(self):
         samFile = pysam.AlignmentFile('HG00096.chrom11.ILLUMINA.bwa.GBR.low_coverage.20120522.bam', "rb")
         print('The proper paired reads are:')
-        for read in samFile.fetch("chr11", self.geneinfo[3], self.geneinfo[4]):
+        for read in samFile.fetch("11", self.geneinfo[3], self.geneinfo[4]):
             if read.is_proper_pair:
                 print("-properly_paired_reads_of_gene:")
                 print(read)
         samFile.close()
 
 
+    ## Indels will be printed out, when in colimn[5] "I" or "D" occur
     def get_gene_reads_with_indels(self):
         samFile = pysam.AlignmentFile('HG00096.chrom11.ILLUMINA.bwa.GBR.low_coverage.20120522.bam', "rb")
         print('Gene_reads_with_indels:')
@@ -89,13 +91,13 @@ class Assignment1:
 
 
     def calculate_gene_average_coverage(self):
-        #a = pybedtools.BedTool('HG00096.chrom11.ILLUMINA.bwa.GBR.low_coverage.20120522.bam')
         a = pybedtools.BedTool(self.geneinfo)
         cov = a.coverage(bg=True)
         print("-gene_average_coverage:")
         print(cov.head())
 
 
+    ## Number mapped reads will be printed out, when 1=0001 -> PE read
     def get_number_mapped_reads(self):
         samFile = pysam.AlignmentFile('HG00096.chrom11.ILLUMINA.bwa.GBR.low_coverage.20120522.bam', "rb")
         n = 0
@@ -110,36 +112,38 @@ class Assignment1:
         samFile.close()
 
 
+    ## Gene Symbol and the [0] position will be printed out
     def get_gene_symbol(self):
-        print("-gene_symbol:", self.geneinfo[0]);
-        return self.geneinfo[0]
+        print("-gene_symbol:")
+        print(self.geneinfo[0])
 
 
+    ## Region of gene will be printed out
     def get_region_of_gene(self):
-        print("-region_of_gene on {} starts with {} and ends with {}".format(self.geneinfo[2], self.geneinfo[3], self.geneinfo[4]))
+        print("-region_of_gene:")
+        print("{} starts with {} and ends with {}".format(self.geneinfo[2], self.geneinfo[3], self.geneinfo[4]))
 
+
+    ## Number of exon and the [6] position will be printed out
     def get_number_of_exons(self):
-        print("-number_of_exons:", self.geneinfo[6]);
-        return self.geneinfo[6]
+        print("-number_of_exons:")
+        print (self.geneinfo[6])
 
 
     def print_summary(self):
         print("All results:")
-        #self.get_sam_header();
-        #self.get_properly_paired_reads_of_gene();
-        #self.get_gene_reads_with_indels();
-        #self.calculate_total_average_coverage();
-        #self.calculate_gene_average_coverage();
-        self.get_number_mapped_reads();
-        self.get_gene_symbol();
-        self.get_region_of_gene();
-        self.get_number_of_exons();
+        self.get_sam_header()
+        self.get_properly_paired_reads_of_gene()
+        #self.get_gene_reads_with_indels();        # run takes long
+        self.calculate_total_average_coverage()    # run takes long
+        #self.calculate_gene_average_coverage()    # run takes long
+        self.get_number_mapped_reads()             # 6396581
+        self.get_gene_symbol()                     # BDNF
+        self.get_region_of_gene()                  # chr11 starts with 27676440 and ends with 27743605
+        self.get_number_of_exons()                 # 2
 
 
 if __name__ == '__main__':
     print ("Assignment 1")
     assignment1 = Assignment1()
     assignment1.print_summary()
-
-
-
